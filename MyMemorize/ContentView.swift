10175f8811ed8fea972bbb226bd690ba7c2f53e0
primeDevansh <dvcam.rastogi1980@gmail.com>
2023-11-27 18:34:56 +0530

    Initial Commit

---
+++ MyMemorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyMemorize
+//
+//  Created by Devansh Rastogi on 27/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMemorize/MyMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemorizeApp.swift
+//  MyMemorize
+//
+//  Created by Devansh Rastogi on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
